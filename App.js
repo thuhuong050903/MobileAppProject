43f196b05c723a92f62b9f0cccf14437d6725dcd
dangtrungps@gmail.com <dangtrungps@gmail.com>
2023-10-04 17:08:01 +0700

    finish header

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from "./src/components/Header"
+import SearchBar from './src/components/SearchBar';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header></Header>
+      <SearchBar></SearchBar>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import Icon from 'react-native-vector-icons/FontAwesome';
+import React from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+
+const Header = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Find Your Favorite Food </Text>
+            <Icon style={styles.icon} name="bell" size={25} color={'#6B50F6'}  />
+        </View>
+    );
+}
+
+export default Header;
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 60,
+        marginLeft: 0,
+        flexDirection: "row",
+        flex: 1,
+        gap:15
+    },
+
+    title: {
+        color: "#22242E",
+        fontSize: 31,
+        marginLeft: -15,
+        width: 250,
+    },
+
+    icon: {
+        marginTop:25,
+    }
+
+});
